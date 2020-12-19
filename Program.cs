9b6005f75b3bd8b55bc9bbe429900e9e12748df2
StefanPant <42117058+StefanPant@users.noreply.github.com>
2020-12-19 21:30:37 +0100

    Add files via upload
    Basic Calculator done in Visual Studio 2017

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator(53, 28);
+            string s = calc.WriteText("Hello World!");
+            Console.WriteLine(s);
+
+            string n = calc.WriteNumber(calc.operand1 + calc.operand2);
+            Console.WriteLine(n);
+
+            Console.Read();
+
+        }
+    }
+
+    class Calculator
+    {
+        public  string WriteText(string a)
+        {
+            return a;
+        }
+
+        public  string WriteNumber(int b)
+
+        {
+            return b.ToString();
+        }
+       public int operand1;
+      public int operand2;
+
+        public Calculator(int operend1, int operend2)
+            {this.operand1 = operend1;
+            this.operand2 = operend2;}
+
+
+
+
+             
+
+
+    }
+
+
+}
